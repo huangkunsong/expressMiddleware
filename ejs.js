794bb6ed725a9b26b04f7280f4500256c2f8248c
huangkunsong <huang_kunsong@sina.cn>
2017-06-04 11:20:40 +0800

    express middleware exercise

--- ejs.js
+++ ejs.js
@@ -2,8 +2,14 @@
  * Created by Huangkunsong on 2017/6/3.
  */
 const express = require("express");
+const path = require("path");
 
 const app = express();
-app.set("view engine");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "ejsViews"));
+
+app.get("/", function (request, response) {
+    response.render("index", {name : 123123123});
+});
 
 app.listen(80);
